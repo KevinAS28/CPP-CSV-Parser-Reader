f906f17096197707109fc108c3625bb25696301b
KevinAS28 <kevinagusto28@gmail.com>
2019-07-11 16:46:20 +0700

    update & fix bugs

--- csv.h
+++ csv.h
@@ -1,95 +1,136 @@
+//CSV parser v 0.2
+//parsing csv format from file name or string
+
+//CSV class:
+
 #ifndef __csv
-#define __csv true
+#define __csv 
 #include <string> //cpp standard library
 #include <vector> //cpp standard library
-#include "strtool.h" //find this on my github: https://github.com/KevinAS28
+#include "strtool.h" 
 #include <map> //cpp standard library
-typedef std::vector<std::string> vstr; //vector string
-typedef std::vector<vstr> vvstr; //vector vector string
+
+
+typedef std::vector<std::string> vstr;
+typedef std::vector<vstr> vvstr;
+typedef std::map<std::string, std::string> mapss;
+typedef std::vector<mapss> DataMap;
+
 class CSV;
 typedef void (CSV::*readerFunc)(std::string, std::string, std::string);
-typedef std::vector<std::map<std::string, std::string>> DataMap;
+
+
 class CSV{
 public:
-	//data
-	vvstr csv_data_vector = vvstr();
-	DataMap csv_data_map = DataMap();
+	//data stored will be here. get your data here after parsing corresponding your parser type.
+	vvstr vector_data = vvstr(); //vector
+	DataMap map_data = DataMap(); //map
+
+	//if you using map parser, here is the column names
+	vstr column_names = vstr();
 
 	//configuration
-	int pass_error = 1;
+	// int pass_error = 1;
 
-	//
 	std::map<std::string, readerFunc> readerMapper = std::map<std::string, readerFunc>();
 	FILE * csv_file;
 	std::string column_splitter = ",";
 	std::string row_splitter = "\n";
-	std::string type = "a";
-	void reader(std::string data, std::string row_splitter="\n",std::string column_splitter = ","){
+
+	//default of parser type
+	std::string type = "v";
+
+	CSV(){
+		//initialize the reader map;
+		this->readerMapper["v"] = &CSV::vectorReader; //vector
+		this->readerMapper["m"] = &CSV::mapReader; //map
+	}
+
+	CSV(std::string data):CSV(){
+		std::string type=this->type, row_splitter=this->row_splitter, column_splitter=this->column_splitter;
+		this->reader(data, type, row_splitter, column_splitter);
+	}
+	CSV(std::string data, std::string type):CSV(){
+		row_splitter=this->row_splitter, column_splitter=this->column_splitter;
+		this->reader(data, type, row_splitter, column_splitter);
+	}		
+	CSV(std::string data, std::string type, std::string row_splitter,std::string column_splitter):CSV(){
+		this->reader(data, type, row_splitter, column_splitter);
+	}
+
+
+	CSV(FILE * the_file):CSV(){
+		std::string type=this->type, row_splitter=this->row_splitter, column_splitter=this->column_splitter, data = this->readFile(the_file);
+		this->reader(data, type, row_splitter, column_splitter);
+	}
+	CSV(FILE * the_file, std::string type):CSV(){
+		std::string data = this->readFile(the_file), row_splitter=this->row_splitter, column_splitter=this->column_splitter;
+		this->reader(data, type, row_splitter, column_splitter);
+	}
+	CSV(FILE * the_file, std::string type, std::string row_splitter,  std::string column_splitter):CSV(){
+		std::string data = this->readFile(the_file);
+		this->reader(data, type, row_splitter, column_splitter);
+	}	
+
+
+
+	std::string readFile(FILE *the_file){
+		std::string data = "";
+		while (true){
+			int d = getc(the_file);
+			if (d==EOF)break;
+			data+=d;
+		}		
+		return data;
+	}
+
+	void reader(std::string data, std::string type, std::string row_splitter,std::string column_splitter){
+		this->type = type;
+		readerFunc func = (this->readerMapper[type]);
+		(this->*func)(data, row_splitter, column_splitter);
+	}
+
+
+	void vectorReader(std::string data, std::string row_splitter ,std::string column_splitter){
 		vstr line = split_str_s(data, row_splitter);
 		this->column_splitter = column_splitter;
 		for (std::string i : line){
 			vstr dat = split_str_s(i, column_splitter);
-			this->csv_data_vector.push_back(dat);
-		}			
-		
+			this->vector_data.push_back(dat);
+		}
 	}
-	void mapReader(std::string data, std::string row_splitter="\n",std::string column_splitter = ","){
+
+
+	void mapReader(std::string data, std::string row_splitter, std::string column_splitter){
 		vstr line = split_str_s(data, row_splitter);
 		if (line.size() <= 1){
 			printf("CSV() -> mapReader(): the row num data should be > 1. because the first row is column name");
 		}
 		this->column_splitter = column_splitter;
 		vstr column = split_str_s(line[0], column_splitter);
+		this->column_names = column;
 		std::map<std::string, std::string> toadd;
 		for (int a = 1; a < line.size(); a++){
 			vstr dat = split_str_s(line[a], column_splitter);
-			if (dat.size()!=column.size()){
-				printf("%d %d %d\n", a, dat.size(), column.size());
-				if (pass_error){
-					continue;
-				}
-			}
+
+			//check the error. 
+			// if (dat.size()!=column.size()){
+			// 	printf("%d %d %d\n", a, dat.size(), column.size());
+			// 	if (pass_error){
+			// 		continue;
+			// 	}
+			// }
+			
 			toadd = std::map<std::string, std::string>();
 			for  (int b = 0; b < column.size(); b++){
 
 				toadd[column[b]] = dat[b];
 			}
-			this->csv_data_map.push_back(toadd);
-		} 
-	}	
-	void reader(std::string data, std::string type="a", std::string row_splitter="\n",std::string column_splitter = ","){
-		this->type = type;
-		readerFunc func = (this->readerMapper[type]);
-		(this->*func)(data, row_splitter, column_splitter);
-	}
-	CSV(){
-		//initialize the mapper;
-		this->readerMapper["a"] = &CSV::reader;
-		this->readerMapper["m"] = &CSV::mapReader;
-	}
-	CSV(std::string data, std::string type="a", std::string row_splitter="\n",std::string column_splitter = ","): CSV(){
-		this->reader(data, type, row_splitter, column_splitter);
-	}
-	CSV(FILE * the_file, std::string type="a", std::string row_splitter="\n",  std::string column_splitter = ","):CSV(){
-		std::string data = "";
-		while (true){
-			int d = getc(the_file);
-			if (d==EOF)break;
-			data+=d;
+			this->map_data.push_back(toadd);
 		}
-		this->reader(data, type, row_splitter, column_splitter);
 	}
-	std::string get_string(){
-		std::string to_return = "";
-		for (vstr v: this->csv_data_vector){
-			for (std::string s:v){
-				to_return+=s;
-				to_return+=this->column_splitter;
-			}
-			to_return+=this->row_splitter;
-		}
-		return to_return;
-	}
+
+
 };
 
 #endif
--- example.cpp
+++ example.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include "csv.h"
 
-void read_from_file(){
-	//with file stream
-	FILE * csv_stream = fopen("house.csv", "r");
-	CSV csv = CSV(csv_stream);
-	std::cout << csv[1][0] << "\n";
-}
+std::string for_vector = "i,read,this,with,vector,parser\nso,i,pull,it,from,\"csv.data_vector();\"";
+std::string for_map = "the,first,row,is,column,\"when using map reader\"\ni,read,this,with,map,parser\nso,i,pull,it,from,\"csv.data_map();\"";
+int main(){	
+	FILE * csv_stream = fopen("house.csv", "r"); 
 
-void from_string(){
-	//with file stream
-	FILE * csv_stream = fopen("house.csv", "r");
-
-	//read the file
-	std::string data = "";
-	int b = 0;
-	while (true){
-		b = getc(csv_stream);
-		if (b==EOF) break;
-		data+=(char)b;
+	//parse csv from existing string using vector parser
+	std::cout << "Reading from string using vector parser: \n";
+	CSV csv0 = CSV(for_vector); // vector parse is the default.
+	for (int row_index = 0; row_index < csv0.vector_data.size(); row_index++){
+		std::cout << row_index << ". ";
+		vstr rowData = csv0.vector_data[row_index];
+		for (int col_index = 0; col_index < rowData.size(); col_index++){
+			std::string data = rowData[col_index];
+			std::cout << data << " ";
+		}
+		std::cout << "\n";
 	}
 
-	//print out the data
-	//std::cout << data;
+	//parse csv from existing string using map parser
+	std::cout << "\nReading from string using map parser: \n";
+	CSV csv1 = CSV(for_map, "m"); // "m" is map parser.
+	vstr column_name = csv1.column_names;
+	for (int row_index = 0; row_index < csv1.map_data.size(); row_index++){
+		std::cout << row_index << ". ";
+		mapss rowData = csv1.map_data[row_index];
+		for (int col_index = 0; col_index < rowData.size(); col_index++){
+			std::string data = rowData[column_name[col_index]];
+			std::cout << column_name[col_index] << " : " << data << ", ";
+		}
+		std::cout << "\n";
+	}
 
-	CSV csv = CSV(data);
-	std::cout << csv[1][0] << "\n";	
-}
+	//parse csv from house.csv using vector parser
+	// std::cout << "\nReading from house.csv using vector parser: \n";
+	// FILE * myfile = fopen("house.csv", "r");
+	// CSV csv2 = CSV(myfile); // vector parse is the default.
+	// for (int row_index = 0; row_index < 3; row_index++){
+	// 	std::cout << row_index << ". ";
+	// 	vstr rowData = csv2.vector_data[row_index];
+	// 	for (int col_index = 0; col_index < rowData.size(); col_index++){
+	// 		std::string data = rowData[col_index];
+	// 		std::cout << data << " ";
+	// 	}
+	// 	std::cout << "\n";
+	// }
 
-int main(){	
- from_string();
- read_from_file();
+	std::cout << "\nReading from house.csv using map parser... why you not try it with your own? :v \n";
+
 	return 0;
 }
